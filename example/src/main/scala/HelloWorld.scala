df1009c4d3213110faa9df499859749f6e19af8b
Shrey Mehta <shrey@dream11.com>
2021-11-09 18:33:48 +0530

    --wip-- [skip ci]

--- example/src/main/scala/HelloWorld.scala
+++ example/src/main/scala/HelloWorld.scala
@@ -1,9 +1,14 @@
+import zhttp.experiment.multipart.{ChunkedData, Message, Parser, Part}
 import zhttp.http._
 import zhttp.service.Server
 import zio._
 import zio.stream._
 
 object HelloWorld extends App {
+  def extractMessage(msg: Message): Chunk[Byte] = msg match {
+    case ChunkedData(chunkedData) => chunkedData
+    case _                        => Chunk.empty
+  }
 
   def h1 = HttpApp.fromHttp {
     Http.collectM[Request] { case req =>
@@ -21,7 +26,46 @@ object HelloWorld extends App {
     }
   }
 
-  def app: HttpApp[Any, Throwable] = h1 +++ h2
+  def h3 = {
+    HttpApp.collectM { case req =>
+      req.decodeContent(ContentDecoder.backPressure).map { content =>
+        val s                            = ZStream.fromQueue(content)
+        println(req.headers)
+        val boundryValue: Option[String] = req.headers.filter(_.name == "Content-Type") match {
+          case ::(head, _) =>
+            println(head.value.toString)
+            head.value.toString.split(";").toList.filter(_.contains("boundary=")) match {
+              case ::(head, _) =>
+                head.split("=").toList match {
+                  case ::(_, next) => Some(next.head)
+                  case Nil         => None
+                }
+              case Nil         => None
+            }
+          case Nil         => None
+        }
+        boundryValue match {
+          case Some(value) =>
+            Response(data =
+              HttpData.fromStream(
+                new Parser(value)
+                  .decodeMultipart(s)
+                  .flatMap {
+                    case Part.Empty           => ZStream.empty
+                    case Part.File(_, d)      =>
+                      d.take(1).map(extractMessage).mapChunks(_.flatten)
+                    case Part.Attribute(_, _) => ZStream.empty
+                  },
+              ),
+            )
+          case None        => Response.status(status = Status.INTERNAL_SERVER_ERROR)
+        }
+
+      }
+    }
+  }
+
+  def app = h3
 
   // Run it like any simple app
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
